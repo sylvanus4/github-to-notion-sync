b5a7a80dace044329237066e4a77d080cfb4f773
Hyojung Han <hyojung.han@thakicloud.co.kr>
2025-10-21 15:04:11 +0900

    feat: Update run script and field mappings
    - Change sprint filter in run.sh to "25-10-Sprint4"
    - Add new field mappings for "ryangkyung-thaki" and "mjhan-tk" in field_mappings.yml

--- run.sh
+++ run.sh
@@ -1,1 +1,2 @@
-PYTHONPATH=. python scripts/complete_resync.py --sprint-filter "25-09-Sprint3" --force
+#PYTHONPATH=. python scripts/complete_resync.py --sprint-filter "25-09-Sprint4" --force
+PYTHONPATH=. python scripts/complete_resync.py --sprint-filter "25-10-Sprint4" --force
